1fa85cb95859b1c582ed74f201af6cce37f30ed1
ishad <mehanizip@mail.ru>
2024-12-01 12:02:30 +0300

    hw12 array

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,14 @@ package org.example;
 
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class HomeWork {
 
@@ -14,6 +20,27 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
+        var bi = new BufferedReader(new InputStreamReader(in));
+        var bo = new PrintStream(out);
+        var count = Integer.parseInt(bi.readLine());
+        for (int i = 0; i < count; i++) {
+            var s = bi.readLine();
+            var max = s.length() + 1;
+            var list = new ArrayList<Integer>();
+            list.add(0);
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == 'R') {
+                    list.add(j + 1);
+                }
+            }
+            list.add(max);
+            var d = 0;
+            for (int j = 1; j < list.size(); j++) {
+                d = Math.max(d, list.get(j) - list.get(j - 1));
+            }
+            bo.println(d);
+        }
+        bo.flush();
     }
 
 
